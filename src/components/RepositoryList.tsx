34164a7257b60bc90f120f3adeee62cd876322bc
Vinícius Monteiro Carvalho <vini.mont.c@gmail.com>
2021-08-01 13:20:16 -0300

    commit react application

---
+++ src/components/RepositoryList.tsx
@@ -0,0 +1,30 @@
+import { RepositoryItem } from "./RepositoryItem";
+
+import '../styles/repositories.scss';
+import { useState, useEffect } from "react";
+
+interface Repository {
+  name: string;
+  description: string;
+  html_url: string;
+}
+
+export function RepositoryList() {
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/vinimontc/repos')
+      .then(response => response.json())
+      .then(data => setRepositories(data))
+  }, [])
+
+  return (
+    <section className="repository-list">
+      <h1>Lista de repositórios do Vinícius</h1>
+
+      <ul>
+        {repositories.map(repository => <RepositoryItem key={repository.name} repository={repository}/>)}
+      </ul>
+    </section>
+  )
+}
